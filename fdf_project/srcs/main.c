f4a5fedfeeea49b4f16fa52e29c26bb5c6a7f380
llama-0 <anrozan93@mail.ru>
2019-09-25 20:54:33 +0300

    typo

--- fdf_project/srcs/main.c
+++ fdf_project/srcs/main.c
@@ -75,7 +75,7 @@ void			new_image(t_data *data)
 	mlx_put_image_to_window(data->win.mlx_ptr, data->win.win_ptr,
 			data->ptr, 5, DH_IM - 150);
 	mlx_string_put(data->win.mlx_ptr, data->win.win_ptr, 5, DH_IM - 50,
-			COLOR_TABLE, "This project is made by super girl's");
+			COLOR_TABLE, "This project is made by super girls");
 	mlx_string_put(data->win.mlx_ptr, data->win.win_ptr, 5, DH_IM - 25,
 			COLOR_TABLE, "Koparker and Bdudley");
 }
